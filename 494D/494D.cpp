b9ea3e90ded0e94fe9995ff199fe0dc0b05ba132
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-12-19 14:34:24 +0100

    starting 494D

---
+++ 494D/494D.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using std::endl;
+using std::cout;
+using std::cin;
+using std::vector;
+using std::pair;
+using std::make_pair;
+
+typedef vector<pair<size_t, size_t> > vss;
+typedef vector<vss> vvss;
+
+vvss tree;
+
+size_t f (size_t first, size_t second) {
+  return first + second;
+}
+
+int main(void) {
+  size_t n;
+  cin >> n;
+  tree.resize(n+1);
+  size_t first, second, weight;
+  for (size_t i = 0; i <n-1; i++) {
+    cin >> first >> second >> weight;
+    tree[first].push_back(make_pair(second, weight));
+    tree[second].push_back(make_pair(first, weight));
+  }
+  
+  size_t q;
+  cin >> q;
+  for (size_t i = 0; i < q; i++) {
+    cin >> first >> second;
+    cout << f(first,second) << endl;
+  }
+
+  return 0;
+}
